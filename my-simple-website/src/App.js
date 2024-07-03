73e877d2b5914a40dfa2d7cbd336ec5a53223693
Amestest <gktk8411@naver.com>
2024-07-03 11:40:58 +0900

    react

---
+++ my-simple-website/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>홈페이지 오신 것을 환영합니다.</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About us</h2>
+          <p>이 홈페이지는 간단한 리액트 홈페이지 입니다.</p>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <p>Email : contact@email.com</p>
+        </section>
+      </main>
+      <footer>
+        <P>&copy; 2024 My website</P>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
